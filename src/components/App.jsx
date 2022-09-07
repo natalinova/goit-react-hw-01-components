9b585d71efac247da3c9ce52319b561db687a523
Nataliya Novak <novak.natali@gmail.com>
2022-09-08 01:28:40 +0300

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,38 @@
-export const App = () => {
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
+import user from './user.json';
+import Profile from "./task_1/Profile"
+ function App() {
+  //  const { username, tag, location, avatar, stat } = user;
+    // const { followers, views, likes } = user.stat;
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        followers={user.stat.followers}
+        views={user.stat.views}
+        likes={user.stat.likes}
+        
+      />
+
+    </>
+)
+ }
+export default App
